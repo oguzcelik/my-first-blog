80331f9531812ea1c082859dc65a0a186eff6a75
Oğuz Çelik <oguzcelik96@gmail.com>
2016-03-19 17:40:19 +0200

    added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 # Create your views here.
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail,name='post_detail'),
 ]
